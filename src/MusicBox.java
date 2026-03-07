66ac4850acab43066072887297b70ebb2485ba1a
sqr1520 <3029814473@qq.com>
2026-03-07 23:15:42 +0800

    Update MusicBox.java

--- src/MusicBox.java
+++ src/MusicBox.java
@@ -1,6 +1,4 @@
-
 public class MusicBox {
-
     // Variables
     private String songID;
     private char isSongPremium;
@@ -19,10 +17,18 @@ public class MusicBox {
         this.songArtists = artists;
         
         // -- TODO
+MusicBox mbox2 = new MusicBox("A12BC34", 'N', "Sunshine", "Bright", 
+    "Happy day", "Pop", "Star Music", "Sunny Vibes");
+
+playsong.playSong(mbox2.getSongID(), mbox2.getPremiumSong(), 1);
         /* Initialise  the rest of the variables in this Constructor block*/
     }
     
-    
+    this.songTitle = title;       // 初始化歌曲标题
+        this.songAlbum = Album;       // 初始化歌曲专辑
+        this.songGenre = genre;       // 初始化歌曲流派
+        this.songProducer = producer; // 初始化歌曲制作人
+        this.songMusicLabel = label;  // 初始化歌曲唱片公司
     //Methods
     public String getSongID() {
         return songID;
@@ -37,8 +43,29 @@ public class MusicBox {
     }
 
 
-    // -- TODO
-        /* Create the rest of "getters" for the remaining variables */
+    public String getSongTitle() {
+        return songTitle;
+    }
+
+    public String getSongAlbum() {
+        return songAlbum;
+    }
+
+    public String getSongGenre() {
+        return songGenre;
+    }
+
+    public String getSongProducer() {
+        return songProducer;
+    }
+
+    public String getSongMusicLabel() {
+        return songMusicLabel;
+    }
+
+    public int getNoAds() {
+        return noAds;
+    }
     
 }
 
