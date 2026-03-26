e56a416f07f7432209617cbc3cef6bb809d03096
sqr1520 <3029814473@qq.com>
2026-03-26 12:53:13 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
      
         public class Main{
     public static void main(String[] args) {
-        
+
+        UserRegistration user = new UserRegistration();
+        user.registration();
+        System.out.println(user);
        AdminPanel adminPanel=new AdminPanel();
          adminPanel.userMangementOptions();
         
