07ca39c89f9e1ae0c138b9feac7e1a4dc2383fe2
sqr1520 <3029814473@qq.com>
2026-03-28 15:34:13 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,14 @@
-     
-        public class Main{
+public class Main{
     public static void main(String[] args) {
+        ERyder bike1 = new ERyder();
+        System.out.println("bike1：");
+        bike1.printBikeDetails(30);
 
+        ERyder bike2 = new ERyder(1001, 80, true, 150.5);
+        System.out.println("bike2：");
+        bike2.ride(10);
+        System.out.println("bike2：");
+        bike2.printBikeDetails(30);
         UserRegistration user = new UserRegistration();
         user.registration();
         System.out.println(user);
@@ -11,7 +18,6 @@
 
     }
 }
-
 
 
 
