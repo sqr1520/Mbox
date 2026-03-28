4236759ed49b0b1eb6d09ce8f1272b7308985da0
sqr1520 <3029814473@qq.com>
2026-03-28 15:37:00 +0800

    Create ActiveRental.java

---
+++ src/ActiveRental.java
@@ -0,0 +1,22 @@
+import java.time.LocalDateTime;
+
+public class ActiveRental {
+
+    public ActiveRental(String bikeID, String emailAddress, LocalDateTime tripStartTime) {
+    }
+
+    public String getBikeID() {
+        throw new UnsupportedOperationException("Unimplemented method 'getBikeID'");
+    }
+
+    public String getEmailAddress() {
+        throw new UnsupportedOperationException("Unimplemented method 'getEmailAddress'");
+    }
+
+    public String getTripStartTime() {
+        throw new UnsupportedOperationException("Unimplemented method 'getTripStartTime'");
+    }
+    
+    
+    
+}
